92a5c61af143cdadb5563c20ab1bbb937219810d
Kirill Leonov <leonov835@yandex.ru>
2024-05-08 23:38:39 +0300

    fix

--- config/config.rb
+++ config/config.rb
@@ -13,6 +13,11 @@ end
 
 Config.load_and_set_settings('config/settings.yaml')
 
+if ENV.fetch('ENVIRONMENT', 'development') == 'production'
+  conf = "#{Settings.wg_path}/wg0.conf"
+  system('wg-quick up wg0') if File.exist?(conf)
+end
+
 require_relative '../lib/wire_guard/server'
 require_relative '../app/clients_controller'
 require_relative '../app/errors/config_not_found_error'
