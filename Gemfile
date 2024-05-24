7a05d1c32881cb7c72a90d538ce7153ac97b61a3
Kirill Leonov <leonov835@yandex.ru>
2024-05-24 21:22:41 +0300

    init branch

--- Gemfile
+++ Gemfile
@@ -10,6 +10,7 @@ gem 'puma', '~> 6.4', '>= 6.4.2'
 gem 'rake', '~> 13.2', '>= 13.2.1'
 gem 'rqrcode', '~> 2.0'
 gem 'sinatra', '~> 4.0'
+gem 'sinatra-contrib', '~> 4.0'
 
 group :development do
   gem 'byebug', '~> 11.1', '>= 11.1.3'
--- app.rb
+++ app.rb
@@ -1,60 +1,64 @@
 # frozen_string_literal: true
 
 require 'sinatra'
+require 'sinatra/contrib'
 
 # Main app class
 class Application < Sinatra::Base
+  register Sinatra::Namespace
+
   AUTH_TOKEN = "Bearer #{ENV.fetch('AUTH_TOKEN', nil)}".freeze
+  ADMIN_PASSWORD = '123'
 
-  before do
-    content_type :json
+  namespace '/api' do # rubocop:disable Metrics/BlockLength
+    before do
+      content_type :json
 
-    pass if request.path == '/healthz'
+      halt 403 unless request.env['HTTP_AUTHORIZATION'] == AUTH_TOKEN
 
-    halt 403 unless request.env['HTTP_AUTHORIZATION'] == AUTH_TOKEN
-
-    @controller = ClientsController.new
-  end
-
-  get '/clients' do
-    controller.index
-  end
-
-  get '/clients/:id' do
-    config = controller.show(params['id'])
-    case params['format']
-    when 'qr'
-      content_type 'image/png'
-
-      send_file Utils::QrCodeBuilder.build(config)
-    when 'conf'
-      content_type 'text/plain'
-
-      Utils::ConfigFileBuilder.build(config)
-    else
-      config
+      @controller = ClientsController.new
     end
-  rescue Errors::ConfigNotFoundError
-    halt 404
-  end
 
-  delete '/clients/:id' do
-    controller.destroy(params['id'])
-  rescue Errors::ConfigNotFoundError
-    halt 404
-  end
+    get '/clients' do
+      controller.index
+    end
 
-  patch '/clients/:id' do
-    controller.update(params['id'], request_body)
-  rescue Errors::ConfigNotFoundError
-    halt 404
-  rescue JSON::Schema::ValidationError => e
-    halt 400, { error: e }.to_json
-  end
+    get '/clients/:id' do
+      config = controller.show(params['id'])
+      case params['format']
+      when 'qr'
+        content_type 'image/png'
 
-  post '/clients' do
-    status 201
-    controller.create(params)
+        send_file Utils::QrCodeBuilder.build(config)
+      when 'conf'
+        content_type 'text/plain'
+
+        Utils::ConfigFileBuilder.build(config)
+      else
+        config
+      end
+    rescue Errors::ConfigNotFoundError
+      halt 404
+    end
+
+    delete '/clients/:id' do
+      controller.destroy(params['id'])
+    rescue Errors::ConfigNotFoundError
+      halt 404
+    end
+
+    patch '/clients/:id' do
+      controller.update(params['id'], request_body)
+    rescue Errors::ConfigNotFoundError
+      halt 404
+    rescue JSON::Schema::ValidationError => e
+      halt 400, { error: e }.to_json
+    end
+
+    post '/clients' do
+      status 201
+      controller.create(params)
+    end
   end
 
   get '/healthz' do
