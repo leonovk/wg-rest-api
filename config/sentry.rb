377da887f4bcf56f8787e513b2856f78218ba9f4
Kirill Leonov <leonov835@yandex.ru>
2024-06-09 21:45:54 +0300

    sentry update config

--- config/sentry.rb
+++ config/sentry.rb
@@ -5,17 +5,7 @@ require 'stackprof'
 
 Sentry.init do |config|
   config.dsn = ENV.fetch('SENTRY_DSN', nil)
-
-  # Set traces_sample_rate to 1.0 to capture 100%
-  # of transactions for performance monitoring.
-  # We recommend adjusting this value in production.
   config.traces_sample_rate = 1.0
-  # or
-  config.traces_sampler = lambda do |_context|
-    true
-  end
-  # Set profiles_sample_rate to profile 100%
-  # of sampled transactions.
-  # We recommend adjusting this value in production.
+  config.traces_sampler = ->(_context) { true }
   config.profiles_sample_rate = 1.0
 end
