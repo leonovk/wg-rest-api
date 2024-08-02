5b18597d3d07e2f79f820d6c8727811b18f0e59c
Kirill Leonov <leonov835@yandex.ru>
2024-08-03 01:14:47 +0300

    fix _+_

--- lib/webhooks/sender.rb
+++ lib/webhooks/sender.rb
@@ -36,7 +36,7 @@ module Webhooks
       result = {}
 
       events.compact.each do |event|
-        arr = result[i] or []
+        arr = result[i] || []
         arr << event
         result[i] = arr
         if i >= 5
