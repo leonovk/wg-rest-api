8057bf0df5f38aebf4322cb52a45ca2350e4bed2
Kirill Leonov <leonov835@yandex.ru>
2025-01-24 01:48:08 +0300

    issue spec

--- spec/lib/wire_guard/client_config_builder_spec.rb
+++ spec/lib/wire_guard/client_config_builder_spec.rb
@@ -14,28 +14,123 @@ RSpec.describe WireGuard::ClientConfigBuilder do
     }
   end
 
-  let(:configs) do
-    {
-      'last_id' => 23,
-      'last_address' => '10.8.0.255'
-    }
-  end
-
-  let(:expected_result) do
-    {
-      id: 24,
-      address: '10.8.1.0',
-      private_key: 'wg_genkey',
-      public_key: 'wg_pubkey',
-      preshared_key: 'wg_genpsk',
-      enable: true,
-      data: {
-        lol: 'kek'
+  context 'when there are no clients on the server' do
+    let(:configs) do
+      {
+        'last_id' => 23,
+        'last_address' => '10.8.0.255'
       }
-    }
+    end
+
+    let(:expected_result) do
+      {
+        id: 24,
+        address: '10.8.1.0',
+        private_key: 'wg_genkey',
+        public_key: 'wg_pubkey',
+        preshared_key: 'wg_genpsk',
+        enable: true,
+        data: {
+          lol: 'kek'
+        }
+      }
+    end
+
+    it 'creates the correct config' do
+      expect(build).to eq(expected_result)
+    end
   end
 
-  it 'creates the correct config' do
-    expect(build).to eq(expected_result)
+  context 'when there is 1 client on the server and the last IP corresponds to it' do
+    let(:configs) do
+      {
+        'last_id' => 1,
+        'last_address' => '10.8.0.2',
+        '1' => {
+          'address' => '10.8.0.2'
+        }
+      }
+    end
+
+    let(:expected_result) do
+      {
+        id: 2,
+        address: '10.8.0.3',
+        private_key: 'wg_genkey',
+        public_key: 'wg_pubkey',
+        preshared_key: 'wg_genpsk',
+        enable: true,
+        data: {
+          lol: 'kek'
+        }
+      }
+    end
+
+    it 'creates the correct config' do
+      expect(build).to eq(expected_result)
+    end
+  end
+
+  context 'when there is 1 client on the server, but the last IP does not match it' do
+    let(:configs) do
+      {
+        'last_id' => 2,
+        'last_address' => '10.8.0.3',
+        '1' => {
+          'address' => '10.8.0.2'
+        }
+      }
+    end
+
+    let(:expected_result) do
+      {
+        id: 3,
+        address: '10.8.0.3',
+        private_key: 'wg_genkey',
+        public_key: 'wg_pubkey',
+        preshared_key: 'wg_genpsk',
+        enable: true,
+        data: {
+          lol: 'kek'
+        }
+      }
+    end
+
+    it 'creates the correct config' do
+      expect(build).to eq(expected_result)
+    end
+  end
+
+  context 'when there are several clients on the server, but there are free IPs between them' do
+    let(:configs) do
+      {
+        'last_id' => 3,
+        'last_address' => '10.8.0.4',
+        '1' => {
+          'address' => '10.8.0.2'
+        },
+        '3' => {
+          'address' => '10.8.0.4'
+        }
+      }
+    end
+
+    let(:expected_result) do
+      {
+        id: 4,
+        address: '10.8.0.3',
+        private_key: 'wg_genkey',
+        public_key: 'wg_pubkey',
+        preshared_key: 'wg_genpsk',
+        enable: true,
+        data: {
+          lol: 'kek'
+        }
+      }
+    end
+
+    it 'creates the correct config' do
+      expect(build).to eq(expected_result)
+    end
   end
 end
