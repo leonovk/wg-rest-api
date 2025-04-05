ac7b5f22f03d8537ced9610e7bb2fb89f4e12ab8
Кирилл Леонов <71232234+leonovk@users.noreply.github.com>
2025-04-05 15:55:57 +0300

    Expand the server API and cover it with tests (#137)

--- app/server/serializer.rb
+++ app/server/serializer.rb
@@ -3,13 +3,31 @@
 module Server
   # server serializer
   class Serializer
-    def serialize(config)
+    def self.serialize(config)
+      new(config).serialize
+    end
+
+    def serialize # rubocop:disable Metrics/MethodLength
       {
         server: config['server'],
         clients_count: config['configs'].except('last_id', 'last_address').size,
-        available_clients_count: available_addresses_count
+        available_clients_count: available_addresses_count,
+        dns: Clients::Serializer::DNS,
+        host: Clients::Serializer::WG_HOST,
+        allowed_ips: Clients::Serializer::WG_ALLOWED_IPS,
+        persistent_keepalive: Clients::Serializer::WG_PERSISTENT_KEEPALIVE,
+        port: Clients::Serializer::WG_PORT,
+        connecting_client_limit: Clients::Serializer::CONNECTING_CLIENT_LIMIT
       }.to_json
     end
+
+    def initialize(config)
+      @config = config
+    end
+
+    private
+
+    attr_reader :config
 
     def available_addresses_count
       (2**(32 - WireGuard::ClientConfigBuilder::CONNECTING_CLIENT_LIMIT)) - 2
---
+++ spec/app/server/serializer_spec.rb
@@ -0,0 +1,27 @@
+# frozen_string_literal: true
+
+RSpec.describe Server::Serializer do
+  describe '#serialize' do
+    subject(:serialize) { described_class.serialize(JSON.parse(File.read('spec/fixtures/wg0.json'))) }
+
+    let(:expected_result) do
+      {
+        'server' => { 'private_key' => '6Mlqg+1Umojm7a4VvgIi+YMp4oPrWNnZ5HLRFu4my2w=',
+                      'public_key' => 'uygGKpQt7gOwrP+bqkiXytafHiM+XqFGc0jtZVJ5bnw=',
+                      'address' => '10.8.0.1' },
+        'clients_count' => 3,
+        'available_clients_count' => 6,
+        'dns' => '1.1.1.1',
+        'host' => '2.2.2.2',
+        'allowed_ips' => '0.0.0.0/0, ::/0',
+        'persistent_keepalive' => 0,
+        'port' => '51820',
+        'connecting_client_limit' => '29'
+      }
+    end
+
+    it 'serializes one config' do
+      expect(JSON.parse(serialize)).to eq(expected_result)
+    end
+  end
+end
